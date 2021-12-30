8126c9d2b97ad05d953a394b4f2539eda47225b4
Nojyto <nojusskirkevicius@gmail.com>
2021-12-31 00:48:48 +0200

    Bug fix

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
-from os import walk
 from datetime import datetime
+
+countOffset = 0
+
+def truncateFileName(name):
+    newFileLen = 20
+    if len(name) > newFileLen:
+        ext = name.split('.')[-1]
+        name = name[:newFileLen - 5 - len(ext)] + "... ." + ext
+    return name
 
 if __name__ == "__main__":
     WORK_DIR = input("Input a working dir: ")
@@ -10,17 +18,21 @@ if __name__ == "__main__":
         exit(-1)
 
     start = datetime.now()
-    dirpath, dirnames, filenames = next(walk(WORK_DIR), (None, None, []))
+    dirpath, dirnames, filenames = next(os.walk(WORK_DIR), (None, None, []))
     fileCount = len(filenames)
 
     print(f"Dir locked. {fileCount} files found.")
     input("\nPress enter to begin...\n")
+    print("Started at", datetime.now().strftime("%H:%M:%S"), "\n")
 
     for i in range(fileCount):
+        curFileNum = i + countOffset
+        maxFileNum = fileCount + countOffset
         curtime = datetime.now().strftime("%H:%M")
-        newname = "{itr:04d}.{ext}".format(itr=i, ext=filenames[i].split('.', 2)[1])
+        newname = f"{curFileNum:04d}.{filenames[i].split('.')[-1]}"
+
         os.rename(os.path.join(dirpath, filenames[i]), os.path.join(dirpath, newname))
 
-        print(f"[{curtime}][{i:04d}/{fileCount:04d}]: {filenames[i]} -> {newname}")
-    
-    print(f"\nFinished in {str(datetime.now() - start).split('.', 2)[0]}")
+        print(f"[{curtime}][{i:04d}/{fileCount:04d}]: {truncateFileName(filenames[i]):>18}  ->  {truncateFileName(newname):>18}")
+
+    print("\nFinished in", str(datetime.now() - start).split('.', 2)[0], "since", datetime.now().strftime("%H:%M:%S"), "\n")
