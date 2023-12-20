f33f9280733efa7bb1a5a45737c0e679ceecde8b
nojyto <nojusskirkevicius@gmail.com>
2023-12-20 23:30:56 +0300

    Refactor file renaming logic and handle file conflicts

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def establish_file(file_name):
     return open(full_path, 'w')
 
 
-def rename_file(old_file_path, file_name, i, prefix=""):
+def rename_file(files, old_file_path, file_name, i, prefix=""):
     root, ext = os.path.splitext(file_name)
     new_file_name = f"{prefix}{i:04d}{ext}"
+    new_root, new_ext = os.path.splitext(new_file_name)
     new_file_path = os.path.join(WORK_PATH, new_file_name)
     
     if new_file_path == old_file_path:
@@ -28,6 +29,10 @@ def rename_file(old_file_path, file_name, i, prefix=""):
     
     if os.path.isfile(new_file_path):
         raise FileExistsError("ERROR: File already exists.")
+    
+    for file in files:
+        if file.startswith(new_root) and file != new_file_name:
+            raise FileExistsError("ERROR: File with different extension already exists.")
     
     os.rename(old_file_path, new_file_path)
     return new_file_name
@@ -48,20 +53,20 @@ if __name__ == "__main__":
         input("\nPress enter to begin...\n")
         start_timestamp = datetime.now()
         print("Started at", start_timestamp.strftime("%H:%M:%S"), "\n")
-        
-        list_dir = os.listdir(WORK_PATH)
-        for i, file_name in enumerate(list_dir, 1):
+    
+        for i in range(file_count):
             curtime = datetime.now().strftime("%H:%M")
+            file_name = files[i]
             old_file_path = os.path.join(WORK_PATH, file_name)
-            log_text = f"[{curtime}][{i:04d}/{file_count:04d}]: {file_name:20}  ->  "
+            log_text = f"[{curtime}][{i:04d}/{(file_count  - 1):04d}]: {file_name:20}  ->  "
             
             try:
-                new_file_name = rename_file(old_file_path, file_name, i, FILE_NAME_PREFIX)
+                new_file_name = rename_file(files, old_file_path, file_name, i, FILE_NAME_PREFIX)
             except Exception as e:
                 rename_status = str(e)
-                if rename_status == "ERROR: File already exists.":
-                    target = list_dir.index(file_name)
-                    list_dir[i - 1], list_dir[target] = list_dir[target], list_dir[i - 1]
+                if rename_status == "ERROR: File already exists." or rename_status == "ERROR: File with different extension already exists.":
+                    target = files.index(file_name)
+                    files[i], files[target] = files[target], files[i]
             else:
                 rename_status = new_file_name
                 
@@ -71,12 +76,17 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\nAborted.")
     except FileNotFoundError as e:
-        print(e)
+        print(f"ERROR: {e}")
+    except PermissionError as e:
+        print("Log.csv is open in another program. Please close it and try again.")
     except Exception as e:
-        print(e)
+        print(f"ERROR: {e}")
     else:
         print("\nFinished in", str(datetime.now() - start_timestamp).split('.', 2)[0], "at", datetime.now().strftime("%H:%M:%S"), "\n")
         print("Log file saved to: ", log_file.name)
     finally:
-        log_file.close()
+        try:
+            log_file.close()
+        except:
+            pass
         print("Exiting...")
