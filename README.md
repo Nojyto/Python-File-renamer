60bd6b80f21288a0891b4ed6a54df16053c9e272
Nojyto <nojusskirkevicius@gmail.com>
2023-12-20 02:17:57 +0200

    Refactor file renaming logic and add logging

---
+++ README.md
@@ -0,0 +1,28 @@
+# Bulk File Renamer
+
+This Python script renames all files in a specified directory in bulk.
+
+## Description
+
+The script prompts the user to input the full path of the target directory. It then renames all files in the directory, and logs the old and new names of each file in a CSV file named `log.csv`.
+
+## Installation
+
+To run the script, you need Python installed on your system. After cloning the repository, you can run the script with the following command:
+
+```bash
+python main.py
+```
+
+## Usage
+
+Here's how to use the script:
+
+1. Run the script using the command python main.py.
+2. When prompted, input the full path of the target directory.
+3. Press enter to begin the renaming process.
+4. The script will then rename all files in the directory and log the changes in log.csv.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- main.py
+++ main.py
@@ -1,38 +1,82 @@
 import os
 from datetime import datetime
 
-countOffset = 0
 
-def truncateFileName(name):
-    newFileLen = 20
-    if len(name) > newFileLen:
-        ext = name.split('.')[-1]
-        name = name[:newFileLen - 5 - len(ext)] + "... ." + ext
-    return name
+EXEC_PATH = os.path.dirname(os.path.abspath(__file__))
+FILE_NAME_PREFIX = ""
+
+
+def establish_file(file_name):
+    full_path = os.path.join(EXEC_PATH, file_name)
+    with open(full_path, 'w') as f:
+        f.seek(0)
+        f.truncate(0)
+        f.close()
+    return open(full_path, 'w')
+
+
+def rename_file(old_file_path, file_name, i, prefix=""):
+    root, ext = os.path.splitext(file_name)
+    new_file_name = f"{prefix}{i:04d}{ext}"
+    new_file_path = os.path.join(WORK_PATH, new_file_name)
+    
+    if new_file_path == old_file_path:
+        raise FileExistsError("ERROR: File name already in correct format.")
+    
+    if not os.path.isfile(old_file_path):
+        raise FileNotFoundError("ERROR: Not a file.")
+    
+    if os.path.isfile(new_file_path):
+        raise FileExistsError("ERROR: File already exists.")
+    
+    os.rename(old_file_path, new_file_path)
+    return new_file_name
+        
 
 if __name__ == "__main__":
-    WORK_DIR = input("Input a working dir: ")
-
-    if not os.path.isdir(WORK_DIR):
-        print("Cannot lock onto directory.\nAborting.")
-        exit(-1)
-
-    start = datetime.now()
-    dirpath, dirnames, filenames = next(os.walk(WORK_DIR), (None, None, []))
-    fileCount = len(filenames)
-
-    print(f"Dir locked. {fileCount} files found.")
-    input("\nPress enter to begin...\n")
-    print("Started at", datetime.now().strftime("%H:%M:%S"), "\n")
-
-    for i in range(fileCount):
-        curFileNum = i + countOffset
-        maxFileNum = fileCount + countOffset
-        curtime = datetime.now().strftime("%H:%M")
-        newname = f"{curFileNum:04d}.{filenames[i].split('.')[-1]}"
-
-        os.rename(os.path.join(dirpath, filenames[i]), os.path.join(dirpath, newname))
-
-        print(f"[{curtime}][{i:04d}/{fileCount:04d}]: {truncateFileName(filenames[i]):>18}  ->  {truncateFileName(newname):>18}")
-
-    print("\nFinished in", str(datetime.now() - start).split('.', 2)[0], "since", datetime.now().strftime("%H:%M:%S"), "\n")
+    try:
+        log_file = establish_file("log.csv")
+        log_file.write("Time, File #, Old Name, New Name\n")
+        
+        WORK_PATH = input("--Bulk file renamer--\nInput target folder full path: ")
+        if not os.path.exists(WORK_PATH):
+            raise FileNotFoundError(f"The directory \'{WORK_PATH}\' does not exist.")
+        files = os.listdir(WORK_PATH)
+        file_count = len(files)
+        
+        print(f"Dir found. {file_count} files located.")
+        input("\nPress enter to begin...\n")
+        start_timestamp = datetime.now()
+        print("Started at", start_timestamp.strftime("%H:%M:%S"), "\n")
+        
+        list_dir = os.listdir(WORK_PATH)
+        for i, file_name in enumerate(list_dir, 1):
+            curtime = datetime.now().strftime("%H:%M")
+            old_file_path = os.path.join(WORK_PATH, file_name)
+            log_text = f"[{curtime}][{i:04d}/{file_count:04d}]: {file_name:20}  ->  "
+            
+            try:
+                new_file_name = rename_file(old_file_path, file_name, i, FILE_NAME_PREFIX)
+            except Exception as e:
+                rename_status = str(e)
+                if rename_status == "ERROR: File already exists.":
+                    target = list_dir.index(file_name)
+                    list_dir[i - 1], list_dir[target] = list_dir[target], list_dir[i - 1]
+            else:
+                rename_status = new_file_name
+                
+            print(log_text + rename_status)
+            log_file.write(', '.join([curtime, str(i), file_name, rename_status]) + '\n')
+                
+    except KeyboardInterrupt:
+        print("\nAborted.")
+    except FileNotFoundError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    else:
+        print("\nFinished in", str(datetime.now() - start_timestamp).split('.', 2)[0], "at", datetime.now().strftime("%H:%M:%S"), "\n")
+        print("Log file saved to: ", log_file.name)
+    finally:
+        log_file.close()
+        print("Exiting...")
